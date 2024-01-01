85d815386d5a8c2ac52de7ca028131f3bba78b11
Shubham-Rasal <bluequbits@gmail.com>
2024-01-01 21:38:45 +0530

    Refactor project and about pages

--- src/content/_project.js
+++ src/content/_project.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Harveeco",
 
     description:
-      "Built during EthIndia Hackathon'23, Harveeco is structured as a DataDAO which allows farmers to monetise and govern their IoT based data. The platform provide zkml proven predictions which is then sold to the big corporations.",
+      "A Farmer's DataDAO.",
 
     live: "https://deano.vercel.app/",
     github: "https://github.com/Shubham-Rasal/Deano",
@@ -14,7 +14,7 @@ export const projects = [
     title: "Deano",
 
     description:
-      "Built during EthOnline Global Hackathon'23, Deano's decentralized ecosystem allows vendors to confidently post data annotation jobs. Annotators earn DAN tokens for accurate labeling, benefiting both parties.",
+      "Decentralised Annotation Network.",
 
     live: "https://deano.vercel.app/",
     github: "https://github.com/Shubham-Rasal/Deano",
@@ -24,7 +24,7 @@ export const projects = [
     title: "Mindful AI",
 
     description:
-      "Built during a hackathon with my friends, mindful Ai is a comprehensive mental illness onset detection system tailored for the needs of universities and colleges with a focus on the mental health of students.",
+      "Mental Illness Detection using AI.",
 
     live: "https://app.supademo.com/demo/2XthgTOFVJYIYPVHqjCtK",
     github: "https://github.com/Shubham-Rasal/Mindful-AI",
@@ -34,7 +34,7 @@ export const projects = [
     title: "dElect",
 
     description:
-      "Using blockchain, voting process can be made more secure, transparent, immutable, and reliable. This project is a decentralized election system that uses blockchain to store the votes and smart contracts to verify the votes.",
+      "Decentralised Voting App.",
 
     live: "https://shubham-rasal.github.io/dElect/",
     github: "https://github.com/shubham-rasal/dElect",
@@ -45,47 +45,17 @@ export const projects = [
     title: "dGame",
 
     description:
-      "This is a multiplayer doodle game where you can draw objects before the other players. It uses socket.io for real-time communication between the clients and the server.",
+      "Multiplayer Doodle Game.",
 
     live: "https://dgame.vercel.app/",
     github: "https://github.com/Shubham-Rasal/wec22-multiplayer-game",
-  },
-  {
-    index: 2,
-    title: "Coder's Dao",
-
-    description:
-      "A decentralized organization that aims to provide a platform for developers to collaborate and build open source projects. It is a community of developers who are passionate about building open source projects.",
-
-    live: "https://shubham-rasal.github.io/coders-dao/",
-    github: "https://github.com/Shubham-Rasal/coders-dao",
-  },
-
-  {
-    index: 4,
-    title: "Twitter Clone",
-
-    description:
-      "An attempt to create the whole of twitter using React and Mongodb. Was successful in replicating the UI and basic backend functionality like posting tweets, liking tweets, etc.",
-
-    github: "https://github.com/Shubham-Rasal/coders-dao",
-  },
-  {
-    index: 5,
-    title: "dMessage",
-
-    description:
-      "One of my first Dapps, this app allows you to send messages to your friends using the Ethereum blockchain. It uses the Rinkeby test network and Metamask for transactions.",
-
-    live: "https://my-waver-app.justideas1.repl.co/",
-    github: "https://github.com/Shubham-Rasal/my-waver-app",
   },
   {
     index: 6,
     title: "iNeural",
 
     description:
-      "A simple neural network library built from scratch in  javascript. It can be used for image classification, regression, and clustering.",
+      "A simple neural network library built from scratch in  javascript.",
 
     github: "https://github.com/Shubham-Rasal/iNeural",
   },
@@ -93,7 +63,7 @@ export const projects = [
     index: 7,
     title: "Perceptron",
 
-    description: "Essentially a single neural node that I have used to demonstate the working of a neural network.",
+    description: "Visualise a signle layer perceptron.",
 
     live: "https://shubham-rasal.github.io/Perceptron/",
     github: "https://github.com/Shubham-Rasal/Perceptron",
